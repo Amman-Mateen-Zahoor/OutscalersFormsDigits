0b1ea048e9535f5b7c05a8bad9c05992b4416520
Aman-Mateen-Zahoor <amman.w4u@gmail.com>
2025-05-20 16:25:21 +0500

    Done Folder Structure having Empty

---
+++ components/TestComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TestComponent = () => {
+  return (
+    <View>
+      <Text>TestComponent</Text>
+    </View>
+  )
+}
+
+export default TestComponent
+
+const styles = StyleSheet.create({})
